de0cc36c165e6755cd28777447bfe0ff74ae3af7
majoreks <s.majorek.konta@gmail.com>
2025-04-06 00:25:40 +0200

    predict

--- src/predict.py
+++ src/predict.py
@@ -3,26 +3,35 @@ from typing import List
 import torch
 
 from model import RegressionModel
-
+from dirs import checkpoint_path
 
 @torch.no_grad()
 def predict(input_features: List[float]):
     # load the checkpoint from the correct path
-    checkpoint = ...
+    checkpoint = torch.load(checkpoint_path)
+
+    input_size = checkpoint.get("input_size")
+    hidden_size = checkpoint.get("hidden_size")
+    x_mean = checkpoint.get("x_mean")
+    x_std = checkpoint.get("x_std")
+    y_mean = checkpoint.get("y_mean")
+    y_std = checkpoint.get("y_std")
+    model_state_dict = checkpoint.get("model_state_dict")
 
     # Instantiate the model and load the state dict
-    model = ...
-    model...
+    model = RegressionModel(input_size, hidden_size)
+    model.load_state_dict(model_state_dict)
+    model.eval()
 
     # Input features is a list of floats. We have to convert it to tensor of the correct shape
     x = torch.tensor(input_features).unsqueeze(0)
 
     # Now we have to do the same normalization we did when training:
-    x = ...
+    x = (x - x_mean) / x_std
 
     # We get the output of the model and we print it
-    output = ...
+    output = model(x)
 
     # We have to revert the target normalization that we did when training:
-    output = ...
+    output = (output * y_std) + y_mean
     print(f"The predicted price is: ${output.item()*1000:.2f}")
