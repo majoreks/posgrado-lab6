4c6a15a418f0a2c86d7b7f339c0f1f8d22115266
Szymon <44604352+majoreks@users.noreply.github.com>
2025-04-05 23:52:01 +0200

    Feature/train (#1)
    * fix reqs
    
    * ignore pycache
    
    * create data loaders
    
    * ignore output
    
    * add matplotlib to requirements
    
    * train model
    
    * save model
    
    * cleanup

--- src/train.py
+++ src/train.py
@@ -5,6 +5,7 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
 from model import RegressionModel
 
@@ -12,35 +13,38 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def train_epoch(dataloader, model, optimizer, criterion):
+    model.train()
 
     # Train the model
     train_loss = 0
     for X, y in dataloader:
-        optimizer...
+        optimizer.zero_grad()
         X, y = X.to(device), y.to(device)
-        y_ = ...
-        loss = ...
+        y_ = model(X)
+        loss = criterion(y_, y)
         train_loss += loss.item() * len(y)
-        loss...
-        optimizer...
+        loss.backward()
+        optimizer.step()
 
     return train_loss / len(dataloader.dataset)
 
 
 def test_epoch(dataloader: DataLoader, model, criterion):
+    model.eval()
+
     test_loss = 0
     for X, y in dataloader:
         X, y = X.to(device), y.to(device)
         with torch.no_grad():
-            y_ = ...
-            loss = ...
+            y_ = model(X)
+            loss = criterion(y_, y)
             test_loss += loss.item() * len(y)
 
     return test_loss / len(dataloader.dataset)
 
 
 def load_data():
-    df = pd.read_csv("/data/housing.csv")
+    df = pd.read_csv("./data/housing.csv")
     train_df, test_df = train_test_split(df, test_size=0.2)
     train_X, train_y = train_df.drop(["ID", "MEDV"], axis=1), train_df["MEDV"]
     test_X, test_y = test_df.drop(["ID", "MEDV"], axis=1), test_df["MEDV"]
@@ -52,8 +56,8 @@ def load_data():
 def train():
 
     # Hyperparameters
-    BATCH_SIZE = 16
-    N_EPOCHS = 10
+    BATCH_SIZE = 128
+    N_EPOCHS = 30
     HIDDEN_SIZE = 64
 
     # This function reads the CSV using pandas and splits it into train and test using sklearn.
@@ -63,44 +67,49 @@ def train():
     # Now you should normalize your data (without Data Leakage) and wrap it in a Dataset
 
     # Start by converting the numpy tensors to torch tensors
-    train_X, train_y = ...
-    test_X, test_y = ...
+    numpy_to_torch = lambda numpy_data: torch.as_tensor(numpy_data, dtype=torch.float32)
+    train_X, train_y = numpy_to_torch(train_X), numpy_to_torch(train_y)
+    test_X, test_y = numpy_to_torch(test_X), numpy_to_torch(test_y)
 
     # Compute the mean and std in the correct dimension
-    x_mean = ...
-    x_std = ...
-    y_mean = ...
-    y_std = ...
+    dim = 0
+    x_mean = train_X.mean(dim=dim)
+    x_std = train_X.std(dim=dim)
+    y_mean = train_y.mean(dim=dim)
+    y_std = train_y.std(dim=dim)
 
     # Apply it to our data ((data - mean)/std)
-    train_X = ...
-    train_y = ...
+    normalize = lambda data, mean, std: (data - mean) / std
+    train_X = normalize(train_X, x_mean, x_std)
+    train_y = normalize(train_y, y_mean, y_std)
 
-    test_X = ...
-    test_y = ...
+    test_X = normalize(test_X, x_mean, x_std)
+    test_y = normalize(test_y, y_mean, y_std)
 
     # Instantiate the datasets
-    train_dataset = ...
-    test_dataset = ...
+    train_dataset = TensorDataset(train_X, train_y)
+    test_dataset = TensorDataset(test_X, test_y)
 
     # Instantiate data loaders
-    train_loader = ...
-    test_loader = ...
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=8)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=8)
 
     input_size = train_X.shape[1]
 
     # Load the model
-    model = ...
+    model = RegressionModel(input_size, HIDDEN_SIZE).to(device)
         
     # You should use a loss function appropriate for regression
-    criterion = ...
+    criterion = torch.nn.MSELoss()
 
     # Setup optimizer. SGD with lr=0.1 will work
-    optimizer = ...
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=0.1)
 
+    train_losses, test_losses = [], []
     for epoch in range(N_EPOCHS):
 
         start_time = time.time()
+        
         train_loss = train_epoch(train_loader, model, optimizer, criterion)
         test_loss = test_epoch(test_loader, model, criterion)
 
@@ -108,27 +117,38 @@ def train():
         mins = secs / 60
         secs = secs % 60
 
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
         print(f"Epoch: {epoch + 1},  | time in {mins} minutes, {secs} seconds")
         print(f'\tLoss: {train_loss:.4f}(train)')
         print(f'\tLoss: {test_loss:.4f}(test)')
 
+    losses_len = range(len(train_losses))
+
+    plt.figure()
+    plt.plot(losses_len, train_losses, 'b', label='Training loss')
+    plt.plot(losses_len, test_losses, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.savefig('./output/loss.png', dpi=400)
+
     # Now save the artifacts of the training
     # Do not change this path (unless debugging). You should mount a Docker volume to it
-    savedir = "/checkpoints/checkpoint.pt"
+    savedir = "./output/checkpoints/checkpoint.pt"
     print(f"Saving checkpoint to {savedir}...")
     # We can save everything we will need later in the checkpoint.
     # Here, we could save a feature transformer if we had used one
     checkpoint = {
-        "model_state_dict": ...,
-        "optimizer_state_dict": ...,
-        "input_size": ...,
-        "hidden_size": ...,
-        "x_mean": ...,
-        "y_mean": ...,
-        "x_std": ...,
-        "y_std": ...,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "input_size": input_size,
+        "hidden_size": HIDDEN_SIZE,
+        "x_mean": x_mean,
+        "y_mean": y_mean,
+        "x_std": x_std,
+        "y_std": y_std,
     }
-    torch.save(...)
+    torch.save(checkpoint, savedir)
 
 if __name__ == "__main__":
     train()
