4252d96a4a85176aefa598db8d3e05ae5d1f35b3
Szymon <44604352+majoreks@users.noreply.github.com>
2025-04-06 00:26:57 +0200

    Feature/predict (#2)
    * refactor checkpoint path
    
    * add info
    
    * predict

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -3,8 +3,10 @@ from train import train
 from predict import predict
 
 if sys.argv[1] == "train":
+    print('train')
     train()
 
 if sys.argv[1] == "predict":
+    print('predict')
     input_features = [float(feat) for feat in sys.argv[2].split(",")]
     predict(input_features)
